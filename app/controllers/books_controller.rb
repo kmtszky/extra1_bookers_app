d098bfa07e816dd8a5203adf10928b1644276cad
kmtszky <kmtszk711@gmail.com>
2021-04-01 04:47:48 +0000

    [Add] pry-bye-bug追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,9 +24,10 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book.params)
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
+      
     else
       render :edit
     end
